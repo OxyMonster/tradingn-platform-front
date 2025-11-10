c80179f4910b63c49042bac55738ebb5e2e93d87
OxyMonster <giorgizeikidze97@gmail.com>
2025-11-10 08:58:10 +0400

    fix styles in trading-terminal

--- src/app/features/client/client.ts
+++ src/app/features/client/client.ts
@@ -1,28 +1,39 @@
 import { Component, PLATFORM_ID, inject, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { isPlatformBrowser } from '@angular/common';
 import { NavigationComponent } from '../../shared/components/navigation/navigation';
 import { FooterComponent } from '../../shared/components/footer/footer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-client',
   standalone: true,
   imports: [RouterOutlet, NavigationComponent, FooterComponent],
   templateUrl: './client.html',
-
   styleUrl: './client.scss',
 })
 export class Client {
+  showFooter = true;
   isLoading = signal(true);
   private platformId = inject(PLATFORM_ID);
   protected readonly title = signal('trading-platform-client');
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const url = event.urlAfterRedirects;
+
+        // Hide footer for all /profile/spot/... routes
+        const shouldHide = /^\/profile\/spot\/[^/]+$/.test(url);
+
+        this.showFooter = !shouldHide;
+      });
+  }
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      // Give auth time to load from localStorage
       setTimeout(() => {
         this.isLoading.set(false);
       }, 50);
